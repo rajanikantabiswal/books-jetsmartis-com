c275273dd0059f46b3e346a2a6d402c09c78546a
RAJANIKANTA BISWAL <rajanikantabiswal15@gmail.com>
2025-01-21 10:42:23 +0530

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -108,7 +108,7 @@
 
         <script>
             $(document).ready(function() {
-                const initialYear = 2024;
+                const initialYear = {{$currentYear}};
                 fetchAndRenderChart(initialYear);
 
 
